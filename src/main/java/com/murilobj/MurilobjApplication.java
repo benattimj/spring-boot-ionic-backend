b95af4c6a05dabd224e9e8342b4c65451ac0fed6
Murilo Benatti <benatti1104@gmail.com>
2021-11-28 22:46:10 +0000

    Projeto Criado

---
+++ src/main/java/com/murilobj/MurilobjApplication.java
@@ -0,0 +1,13 @@
+package com.murilobj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurilobjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MurilobjApplication.class, args);
+	}
+
+}
