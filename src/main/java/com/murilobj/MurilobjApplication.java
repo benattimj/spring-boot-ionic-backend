3aefd1b1396719d7af3f17f2e9e6b5397d10b862
Murilo Benatti <benatti1104@gmail.com>
2021-12-12 00:03:43 +0000

    Paging with parameters in the acquisition

--- src/main/java/com/murilobj/MurilobjApplication.java
+++ src/main/java/com/murilobj/MurilobjApplication.java
@@ -64,6 +64,11 @@ public class MurilobjApplication implements CommandLineRunner {
 		
 		Categoria cat1 = new Categoria ("Informática", null);
 		Categoria cat2 = new Categoria ("Escritório",null );
+		Categoria cat3 = new Categoria ("Bed Shower ",null );
+		Categoria cat4 = new Categoria ("Electronics",null );
+		Categoria cat5 = new Categoria ("Garden",null );
+		Categoria cat6 = new Categoria ("Decoration",null );
+		Categoria cat7 = new Categoria ("Perfum",null );
 		
 		Produto p1 = new Produto(null, "Computer", 2000.00);
 		Produto p2 = new Produto(null, "Printer", 800.00);
@@ -76,7 +81,7 @@ public class MurilobjApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 	
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 
 		Estado est1 = new Estado(null, "Minas Gerais");
